d1b2f4f1424fe718992d13080c6db5d055159a32
Shivam Rai <shivam.r@inspironlabs.com>
2024-10-08 20:15:50 +0530

    third commit

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,16 +1,16 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 export const sequelize = new Sequelize(
-    "postgres://postgres:Shivam@321@localhost:5432/demo",
-    {
-        dialect: "postgres",
-    }                                       
-)
+  "postgres://postgres:Shivam@321@localhost:5432/demo",
+  {
+    dialect: "postgres",
+  }
+);
 
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch((err: any )=> {
-        console.error('Unable to connect to the database:', err);
-    })
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err: any) => {
+    console.error("Unable to connect to the database:", err);
+  });
--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import { sequelize } from './config/db';
-import userRouter from './routes/userRoutes';
-var cors = require('cors')
+import express from "express";
+import { sequelize } from "./config/db";
+import userRouter from "./routes/userRoutes";
+var cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 const PORT = 2000;
-app.use('/',userRouter);
+app.use("/", userRouter);
 
 app.listen(PORT, async () => {
-    try {
-        await sequelize.sync();
-        console.log("Database connected");
-    } catch (error) {
-        console.log(error)
-    }
-    console.log(`http://localhost:${PORT}`)
-})
+  try {
+    await sequelize.sync();
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`http://localhost:${PORT}`);
+});
